0370a4ac50e64a4c98432fb814eacf57413ffc77
kixanity <marky.acoba@gmail.com>
2021-11-17 11:12:37 +0800

    tite

---
+++ Beginner/day-2-3.py
@@ -0,0 +1,11 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?\n")
+# 🚨 Don't change the code above 👆
+
+# Write your code below this line 👇
+left = 90 - int(age)
+days = left * 365
+weeks = left * 52
+months = left * 12
+
+print(f"You have {days} days, {weeks} weeks, and {months} months left.")
